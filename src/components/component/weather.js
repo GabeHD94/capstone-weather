067214b7ab7f53377aedf0d1b42a429f4f98e8a1
GabeHD94 <gabehd94@gmail.com>
2020-07-27 09:35:11 -0600

    added hi

--- src/components/component/weather.js
+++ src/components/component/weather.js
@@ -18,7 +18,7 @@ function Data(props) {
   if (!Cookies.get("username")) {
     props.history.push("/")
   }
-  
+
 
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
@@ -34,103 +34,104 @@ function Data(props) {
         })
     }
   }
-   
+
 
   return (
 
     <div className="weatherdata">
-
-    <div className="weatherApp">
-
-      <main className="enter">
-        <div className="searchBox">
-          <input
-            type="text"
-            className="searchBar"
-            placeholder="Enter City"
-            onChange={event => setQuery(event.target.value)}
-            value={query}
-            onKeyPress={search}
-          />
-        </div>
-        <div>
-          {(typeof weather.main != "undefined") ? (
-            <Spring
-              from={{ opacity: 0, marginTop: -500 }}
-              to={{ opacity: 1, marginTop: 0 }}
-            >
-              {props => (
-                <div style={props}>
-                  <div>
-                    <div className='app'>
-                      <div className="date-wrapper">
-                        <Date />
-                      </div>
-                      <div className="clock-wrapper">
-                        <Clock />
-                      </div>
-                      <Google />
-
-                    </div>
-
-
-                    <div className="weather-box">
-                      <div className="temp">
-                        {Math.round(weather.main.temp)}°F
-                </div>
-                      <div className="weather">
-                        {weather.weather[0].main}
-                      </div>
-
-                      <div className="location-box">
-                        <div className='location'>{weather.name}
-                              
+      <div className="weatherApp">
+        <main className="enter">
+          <div className="searchBox">
+            <input
+              type="text"
+              className="searchBar"
+              placeholder="Enter City"
+              onChange={event => setQuery(event.target.value)}
+              value={query}
+              onKeyPress={search}
+            />
+          </div>
+          <div>
+            {(typeof weather.main != "undefined") ? (
+              <Spring
+                from={{ opacity: 0, marginTop: -500 }}
+                to={{ opacity: 1, marginTop: 0 }}
+              >
+                {props => (
+                  <div style={props}>
+                    <div>
+                      <div className='app'>
+                        <div className="date-wrapper">
+                          <Date />
                         </div>
-                        
+                        <div className="clock-wrapper">
+                          <Clock />
+                        </div>
+                        <div className="hi-wrap">
+                          <h1>Hi {Cookies.get("username")}!</h1>
+                        </div>
+                        <Google />
+
                       </div>
 
 
-                    </div>
-                    <div className="weatherDetails" >
-                      <div className="windSpeed">
-                        <h1>Wind Speed</h1>
-                        <div className="wind">
-                          {Math.round(weather.wind.speed)} mph
-                    </div>
+                      <div className="weather-box">
+                        <div className="temp">
+                          {Math.round(weather.main.temp)}°F
+                </div>
+                        <div className="weather">
+                          {weather.weather[0].main}
+                        </div>
+
+                        <div className="location-box">
+                          <div className='location'>{weather.name}
+
+                          </div>
+
+                        </div>
+
+
                       </div>
-                      <div className="humidity">
-                        <h1>Humidity</h1>
-                        <div className="humdata">
-                          {weather.main.humidity}%
+                      <div className="weatherDetails" >
+                        <div className="windSpeed">
+                          <h1>Wind Speed</h1>
+                          <div className="wind">
+                            {Math.round(weather.wind.speed)} mph
+                    </div>
+                        </div>
+                        <div className="humidity">
+                          <h1>Humidity</h1>
+                          <div className="humdata">
+                            {weather.main.humidity}%
                   </div>
-                      </div>
-                      <div className="tempDetails">
-                        <div className="temps">
-                          <div className="min">
-                            <h1>Min</h1>
-                            <div className="minsize">
-                              {Math.round(weather.main.temp_min)}°F
+                        </div>
+                        <div className="tempDetails">
+                          <div className="temps">
+                            <div className="min">
+                              <h1>Min</h1>
+                              <div className="minsize">
+                                {Math.round(weather.main.temp_min)}°F
+                            </div>
+                            </div>
+                            <div className="max">
+                              <h1>Max</h1>
+                              <div className="maxsize">
+                                {Math.round(weather.main.temp_max)}°F
+                          </div>
                             </div>
                           </div>
-                          <div className="max">
-                            <h1>Max</h1>
-                            <div className="maxsize">
-                              {Math.round(weather.main.temp_max)}°F
-                          </div>
-                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              )
-            }
-            </Spring >
-          ) : (<h2>Enter a city</h2>)}
-        </div>
-      </main>
+                )
+                }
+              </Spring >
+            ) : (<h2>Enter a city</h2>)}
+          </div>
+        </main>
+      </div>
     </div>
-  </div>
   );
 
 
